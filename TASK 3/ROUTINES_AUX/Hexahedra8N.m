eccef63c11999afc3fa1e090d638fd274d71741f
MikoMikarro <lopezcuestam@gmail.com>
2022-12-26 12:48:51 +0100

    Added Hexahedra8N

---
+++ TASK 3/ROUTINES_AUX/Hexahedra8N.m
@@ -0,0 +1,25 @@
+function  [NeXi, BeXi] = Hexahedra8N(xi)
+% Shape functions and derivatives for 2-node linear element 
+ 
+% Matrix of shape functions
+e1 = xi(1);
+e2 = xi(2);
+e3 = xi(3);
+p1 = 1+e1;
+p2 = 1+e2; 
+p3 = 1+e3;
+n1 = 1-e1;
+n2 = 1-e2;
+n3 = 1-e3;
+
+NeXi =(1/8)*[n1*n2*n3 p1*n2*n3 p1*p2*n3 n1*p2*n3 ...
+             n1*n2*p3 p1*n2*p3 p1*p2*p3 n1*p2*p3];
+% Matrix of the gradient of shape functions 
+BeXi = zeros(3,8);
+BeXi(:, 1:4) = [-n2*n3  n2*n3  p2*n3 -p2*n3; ...
+                -n1*n3 -p1*n3  p1*n3  n1*n3; ...
+                -n1*n2 -p1*n2 -p1*n2 -n1*p2] * 1/8;
+BeXi(:, 5:8) = [-n2*p3  n2*p3  p2*p3 -p2*p3; ...
+                -n1*p3 -p1*p3  p1*p3  n1*p3; ...
+                 n1*n2  p1*n2  p1*p2  n1*p2] * 1/8;
+            
---
+++ TASK 3/ROUTINES_AUX/Hexahedra8NInPoints.m
@@ -0,0 +1,28 @@
+function [weig,posgp,shapef,dershapef] = Hexahedra8NInPoints()
+% This function returns, for each 2-node 1D linear element, % Esta
+% definicion no esta bien 
+% and using a 1 points Gauss rule (ngaus=1): , if TypeIntegrand = K
+% and ngaus=2, if TypeIntegrand = RHS:
+%
+% weig = Vector of Gauss weights (1xngaus)
+% posgp: Position of Gauss points  (ndim x ngaus)
+% shapef: Array of shape functions (ngaus x nnodeE)
+% dershape: Array with the derivatives of shape functions, with respect to
+% element coordinates (ndim x nnodeE x ngaus)
+
+weig  = [1, 1, 1, 1, 1, 1, 1, 1] ;
+posgp = 1/sqrt(3)*[-1 -1 -1; 1 -1 -1; 1 1 -1; -1 1 -1; ...
+                   -1 -1  1; 1 -1  1; 1 1  1; -1 1  1] ;
+
+
+ndim = 3; 
+nnodeE = 8;
+ngaus = length(weig) ;
+shapef = zeros(ngaus,nnodeE) ;
+dershapef = zeros(ndim,nnodeE,ngaus) ;
+for g=1:length(weig)
+    xi = posgp(g,:) ;
+    [Ne, BeXi] = Hexahedra8N(xi) ;
+    shapef(g,:) = Ne ;
+    dershapef(:,:,g) = BeXi ;
+end
