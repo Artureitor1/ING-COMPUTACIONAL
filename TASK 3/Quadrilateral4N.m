1de552e31f310ae6c89d9ae628ecd7934a2064ef
MikoMikarro <lopezcuestam@gmail.com>
2022-12-26 12:13:41 +0100

    add 2d shape functions

---
+++ TASK 3/Quadrilateral4N.m
@@ -0,0 +1,7 @@
+function  [NeXi BeXi] = Quadrilateral4N(xi); 
+% Shape functions and derivatives for 2-node linear element 
+ 
+% Matrix of shape functions
+NeXi =0.25*[(1-xi(1))*(1-xi(2)) (1+xi(1))*(1-xi(2)) (1+xi(1))*(1+xi(2)) (1-xi(1))*(1+xi(2))];
+% Matrix of the gradient of shape functions 
+BeXi = 0.25*[-(1-xi(2)) (1-xi(2)) (1+xi(2)) -(1+xi(2));-(1-xi(1)) -(1+xi(1)) (1+xi(1)) (1-xi(1))]; 
